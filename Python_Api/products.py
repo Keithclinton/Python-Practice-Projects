388010e14ea93b80ba9cc0b237f124b6f84c41d2
Keithclinton <keithnyaburi@gmail.com>
2024-06-08 15:55:41 +0300

    Uisng HTTP requests to CRUD in a REST Api

---
+++ Python_Api/products.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+import json
+app = Flask("Product Server")
+products = [
+    {'id': 143, 'name': 'notebook', 'price': 55},
+    {'id': 144, 'name': 'Black Marker', 'price': 78}
+]
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(products)
+@app.route('/products/<id>', methods=['GET'])
+def get_product(id):
+    id = int(id)
+    product = [x for x in products if x['id'] == id][0]
+    return jsonify(product)
+@app.route('/products', methods=['POST'])
+def add_product():
+    products.append(request.get_json())
+    return '', 201
+@app.route('/products/<id>', methods=['PUT'])
+def update_product(id):
+    id = int(id)
+    updated_product = json.loads(request.data)
+    product = [x for x in products if x['id'] == id][0]
+    for key,value in updated_product.items():
+        product[key] = value
+    return '', 204
+@app.route('/products/<id>', methods=['DELETE'])
+def remove_product(id):
+    id = int(id) 
+    product = [x for x in products if x['id'] == id][0] 
+    products.remove(product)
+    return '', 204
+app.run(port=5000,debug=True)  
